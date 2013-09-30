e246bc33ee1443c3ab000890b7dc90954c299e53
Gabe Fierro <gtfierro225@gmail.com>
2013-09-30 09:50:16 -0700

    go fmt

--- cross_validate.go
+++ cross_validate.go
@@ -1,45 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-    "os"
-    "github.com/gtfierro/patentcluster"
-    "bufio"
-    "strconv"
+	"github.com/gtfierro/patentcluster"
+	"os"
+	"strconv"
 )
 
-
 func main() {
-    outfile, err := os.Create("cross_validation.csv")
-    if err != nil {
-        fmt.Println("Could not output to file cross_validation.csv", err)
-        return
-    }
-    defer outfile.Close()
-    writer := bufio.NewWriter(outfile)
-    filename := os.Args[1]
-    fmt.Println("reading from file", filename)
-    data := patentcluster.Extract_file_contents(filename, true)
-    fmt.Println(len(patentcluster.Tagset), "unique tags")
-    patents := patentcluster.Make_patents(data)
-    fmt.Println(len(patents), "unique patents")
-    writer.WriteString("Epsilon, MinPts, Number of Clusters, Mean Cluster Size, Median Cluster Size, Largest Cluster Size\n")
-    for epsilon := .5; epsilon <= .95; epsilon += .05 {
-        for minpts := 3; minpts < 6; minpts += 1 {
-            fmt.Println("Using epsilon =", epsilon,"and minpts =", minpts)
-            fmt.Println("Initializing DBSCAN...")
-            db := patentcluster.Init_DBSCAN(patents, epsilon, minpts)
-            fmt.Println("Running DBSCAN...")
-            db.Run()
-            fmt.Println("Finished running DBSCAN. Computing Stats...")
-            num_clusters, mean_size, median_size, largest, _ := db.Compute_Stats()
-            fmt.Println("Number of clusters found:", num_clusters)
-            fmt.Println("Mean cluster size:", mean_size)
-            fmt.Println("Median cluster size:", median_size)
-            fmt.Println("Largest cluster size:", largest)
-            line := strconv.FormatFloat(epsilon, 'g', -1, 64) + ", " + strconv.Itoa(minpts) + ", " + strconv.Itoa(num_clusters) +", " + strconv.FormatFloat(mean_size, 'g', -1, 64) + ", " + strconv.Itoa(median_size) + ", " + strconv.Itoa(largest) + "\n"
-            writer.WriteString(line)
-        }
-    }
-    writer.Flush()
+	outfile, err := os.Create("cross_validation.csv")
+	if err != nil {
+		fmt.Println("Could not output to file cross_validation.csv", err)
+		return
+	}
+	defer outfile.Close()
+	writer := bufio.NewWriter(outfile)
+	filename := os.Args[1]
+	fmt.Println("reading from file", filename)
+	data := patentcluster.Extract_file_contents(filename, true)
+	fmt.Println(len(patentcluster.Tagset), "unique tags")
+	patents := patentcluster.Make_patents(data)
+	fmt.Println(len(patents), "unique patents")
+	writer.WriteString("Epsilon, MinPts, Number of Clusters, Mean Cluster Size, Median Cluster Size, Largest Cluster Size\n")
+	for epsilon := .5; epsilon <= .95; epsilon += .05 {
+		for minpts := 3; minpts < 6; minpts += 1 {
+			fmt.Println("Using epsilon =", epsilon, "and minpts =", minpts)
+			fmt.Println("Initializing DBSCAN...")
+			db := patentcluster.Init_DBSCAN(patents, epsilon, minpts)
+			fmt.Println("Running DBSCAN...")
+			db.Run()
+			fmt.Println("Finished running DBSCAN. Computing Stats...")
+			num_clusters, mean_size, median_size, largest, _ := db.Compute_Stats()
+			fmt.Println("Number of clusters found:", num_clusters)
+			fmt.Println("Mean cluster size:", mean_size)
+			fmt.Println("Median cluster size:", median_size)
+			fmt.Println("Largest cluster size:", largest)
+			line := strconv.FormatFloat(epsilon, 'g', -1, 64) + ", " + strconv.Itoa(minpts) + ", " + strconv.Itoa(num_clusters) + ", " + strconv.FormatFloat(mean_size, 'g', -1, 64) + ", " + strconv.Itoa(median_size) + ", " + strconv.Itoa(largest) + "\n"
+			writer.WriteString(line)
+		}
+	}
+	writer.Flush()
 }
--- pattag.go
+++ pattag.go
@@ -1,53 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-    "strings"
-    "os"
-    "github.com/gtfierro/patentcluster"
-    "runtime/pprof"
-    "log"
-    "flag"
+	"github.com/gtfierro/patentcluster"
+	"log"
+	"os"
+	"runtime/pprof"
+	"strings"
 )
+
 var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
-
 func practice_run(db *patentcluster.DBSCAN, patents [](*patentcluster.Patent)) {
-    for _, pat := range patents {
-        seeds := db.RegionQuery(pat)
-        if len(seeds) > 0 {
-            fmt.Println(len(seeds))
-        }
-    }
+	for _, pat := range patents {
+		seeds := db.RegionQuery(pat)
+		if len(seeds) > 0 {
+			fmt.Println(len(seeds))
+		}
+	}
 }
 
 func main() {
-    flag.Parse()
-    if *cpuprofile != "" {
-        f, err := os.Create(*cpuprofile)
-        if err != nil {
-            log.Fatal(err)
-        }
-        pprof.StartCPUProfile(f)
-        defer pprof.StopCPUProfile()
-    }
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
-    filename := os.Args[1]
-    rootname := strings.Split(filename, ".")[0]
-    fmt.Println("reading from file", filename)
-    data := patentcluster.Extract_file_contents(filename, true)
-    fmt.Println(len(patentcluster.Tagset), "unique tags")
-    patents := patentcluster.Make_patents(data)
-    fmt.Println(len(patents), "unique patents")
-    fmt.Println("Initializing DBSCAN...")
-    db := patentcluster.Init_DBSCAN(patents, .6, 4)
-    fmt.Println("Running DBSCAN...")
-    db.Run()
-    fmt.Println("Finished running DBSCAN. Computing Stats...")
-    num_clusters, mean_size, median_size, largest, _ := db.Compute_Stats()
-    fmt.Println("Number of clusters found:", num_clusters)
-    fmt.Println("Mean cluster size:", mean_size)
-    fmt.Println("Median cluster size:", median_size)
-    fmt.Println("Largest cluster size:", largest)
-    db.To_file(rootname+".out", true)
+	filename := os.Args[1]
+	rootname := strings.Split(filename, ".")[0]
+	fmt.Println("reading from file", filename)
+	data := patentcluster.Extract_file_contents(filename, true)
+	fmt.Println(len(patentcluster.Tagset), "unique tags")
+	patents := patentcluster.Make_patents(data)
+	fmt.Println(len(patents), "unique patents")
+	fmt.Println("Initializing DBSCAN...")
+	db := patentcluster.Init_DBSCAN(patents, .6, 4)
+	fmt.Println("Running DBSCAN...")
+	db.Run()
+	fmt.Println("Finished running DBSCAN. Computing Stats...")
+	num_clusters, mean_size, median_size, largest, _ := db.Compute_Stats()
+	fmt.Println("Number of clusters found:", num_clusters)
+	fmt.Println("Mean cluster size:", mean_size)
+	fmt.Println("Median cluster size:", median_size)
+	fmt.Println("Largest cluster size:", largest)
+	db.To_file(rootname+".out", true)
 }

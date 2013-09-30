ca3753bec88abd80b3f0cd40996fbc66d2fe1fa0
Gabe Fierro <gtfierro225@gmail.com>
2013-09-30 12:04:03 -0700

    adjust code to account for new patentcluster api

--- pattag.go
+++ pattag.go
@@ -35,9 +35,8 @@ func main() {
 	filename := os.Args[1]
 	rootname := strings.Split(filename, ".")[0]
 	fmt.Println("reading from file", filename)
-	data := patentcluster.Extract_file_contents(filename, true)
+	patents := patentcluster.Extract_file_contents(filename, true)
 	fmt.Println(len(patentcluster.Tagset), "unique tags")
-	patents := patentcluster.Make_patents(data)
 	fmt.Println(len(patents), "unique patents")
 	fmt.Println("Initializing DBSCAN...")
 	db := patentcluster.Init_DBSCAN(patents, .6, 4)
